6dfe2b4816562c23a6cc36c54fd5e38f937d964a
Azhar <azharumi786@gmail.com>
2022-08-27 20:26:43 +0530

    added UC1

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,9 +1,29 @@
 package com.bl.employewagemanagement;
 
+import java.util.Random;
+
 public class EmployeeWageComputation {
 
 		public static void main(String[] args) {
 			
-			System.out.println("Welcome to Employee Wage Computation");
+			int num = 1; 
+			
+			Random rn = new Random();
+			
+			int empType = rn.nextInt(2);
+		
+			System.out.println(empType);
+					
+			if (empType == num) {
+				
+				System.out.println("Employee is present");
+			}
+			else {
+					
+				System.out.println("Employee is absent");
+				}
+		}	
+	
 		}
-}
+
+
