86b28e10e5832e5253be00db898fc2af04d1455e
Azhar <azharumi786@gmail.com>
2022-08-27 20:19:09 +0530

    added employeeage

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,9 @@
+package com.bl.employewagemanagement;
+
+public class EmployeeWageComputation {
+
+		public static void main(String[] args) {
+			
+			System.out.println("Welcome to Employee Wage Computation");
+		}
+}
