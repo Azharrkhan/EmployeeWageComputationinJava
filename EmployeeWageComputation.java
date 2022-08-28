6c0716df29b9d7cb4ab2aaa1191f7533d4deaa84
Azhar <azharumi786@gmail.com>
2022-08-28 09:09:53 +0530

    added UC3

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,34 +4,30 @@ import java.util.Random;
 
 public class EmployeeWageComputation {
 
-		public static void main(String[] args) {
-			
-			System.out.println("Welcome to Employee Wage Computation Program");
-			
-			int num = 1;
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Employee Wage Computation");
+
+		int num = 1;
+		Random random = new Random();
+
+		int empType = random.nextInt(3);
+
+		System.out.println(empType);
+
+		if (empType < num) {
+
+			System.out.println("Employee is absent");
+		} else if (empType == num) {
+
+			System.out.println("Employee is present");
+		} else {
+			System.out.println("Employee is partime");
+
 			int wagePerHour = 20;
-			int fullDayHour = 8;
-			int dailyEmployeeWage;
-			
-			Random rn = new Random();
-			
-			int empType = rn.nextInt(2);
-		
-			System.out.println(empType);
-					
-			if (empType == num) {
-				
-				System.out.println("Employee is present");
-					
-				dailyEmployeeWage = wagePerHour * fullDayHour;
-				
-				System.out.println("Employee daily wage " + dailyEmployeeWage);
-			}
-			else {
-					
-				System.out.println("Employee is absent");
-				}
-		}	
+			int fullDayHour = 4;
+
+			System.out.println("dailyEmployeeWage" + " is " + wagePerHour * fullDayHour);
 		}
-
-
+	}
+}
