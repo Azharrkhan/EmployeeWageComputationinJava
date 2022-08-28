6e6307dc85f41df43cca5c97e94b0e7493dbbb84
Azhar <azharumi786@gmail.com>
2022-08-28 08:46:39 +0530

    updated employeewagecomputation

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -6,6 +6,8 @@ public class EmployeeWageComputation {
 
 		public static void main(String[] args) {
 			
+			System.out.println("Welcome to Employee Wage Computation Program");
+			
 			int num = 1; 
 			
 			Random rn = new Random();
