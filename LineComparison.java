fd2e5296e64fccd9f659fac3cec786f95920bf06
Azhar <azharumi786@gmail.com>
2022-08-30 12:38:12 +0530

    Added line

---
+++ LineComparison.java
@@ -0,0 +1,13 @@
+package com.bl.employewagemanagement;
+
+import java.util.Scanner;
+
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Program");
+		
+	}
+}
