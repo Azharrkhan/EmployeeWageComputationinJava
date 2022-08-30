28c1e9377b0f7887f4c7c9cd7d6b4e2f35f3dc98
Azhar <azharumi786@gmail.com>
2022-08-30 12:04:45 +0530

    added UC6

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,40 +4,49 @@ import java.util.Random;
 
 public class EmployeeWageComputation {
 
-	public static void main(String[] args) {
+		public static void main(String[] args) {
+			System.out.println("Welcome to Employee Wage Computation Program");
 
-		System.out.println("Welcome to Employee Wage Computation Program");
+			int wage = 0;
+			int empHour = 0;
+			int empWagePerHour = 20;
+			int noOfDays = 20;
+			final byte fullTime = 1;
+			final byte partTime = 2;
+			int monthlyHours = 100;
+			int monthlySalary = 0;
 
-		int wage = 0;
-		int empHour = 0;
-		int empWagePerHour = 20;
-		int noOfDays = 20;
-		final byte fullTime = 1;
-		final byte partTime = 2;
+			while ((monthlyHours > 0) && noOfDays > 0) {
 
-		Random rn = new Random();
+			Random rn = new Random();
 
-		int empType = rn.nextInt(3);
+			int empType = rn.nextInt(3);
 
-		System.out.println(empType);
+			System.out.println(monthlySalary);
 
-		switch (empType) {
-		case fullTime:
-			empHour = 8;
-			wage = empWagePerHour * empHour * noOfDays;
-			System.out.println("Monthly wage of present employee is " + wage);
-			break;
+			switch (empType) {
+			case fullTime:
+				empHour = 8;
+				monthlyHours = monthlyHours - empHour;
+				noOfDays -= 1;
+				wage = empWagePerHour * empHour;
+				monthlySalary += wage;
+				break;
 
-		case partTime:
-			empHour = 4;
-			wage = empWagePerHour * empHour * noOfDays;
-			System.out.println("Monthly wage of part time employee is " + wage);
-			break;
+			case partTime:
+				empHour = 4;
+				monthlyHours = monthlyHours - empHour;
+				noOfDays -= 1;
+				wage = empWagePerHour * empHour;
+				monthlySalary += wage;
+				break;
 
-		default:
-			wage = 0;
-			System.out.println("Monthly wage of absent employee is " + wage);
-			break;
+			default:
+				wage = 0;
+				break;
+			}
+			System.out.println(noOfDays + " " + monthlyHours + " " + monthlySalary);
+			}
+			System.out.println("Monthly salary of employee is : " + monthlySalary);
 		}
 	}
-}
